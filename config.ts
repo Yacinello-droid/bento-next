ad87d4f076af6422c19508200fc8aac20563ea1b
Yacinello <136171795+Yacinello-droid@users.noreply.github.com>
2024-10-27 19:35:25 +0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -226,7 +226,7 @@ export const config: Config = {
         ],
       },
       {
-        icon: 'reddit',
+        icon: 'fab-reddit',
         links: [
           {
             name: 'CommandLine',
